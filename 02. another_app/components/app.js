206494f851e84d3ea38702d25ffb8874fa7fd23d
Krystian Petek <krystianpetek2@gmail.com>
2022-08-14 19:02:30 +0200

    complete input display simultanously task

--- 02. another_app/components/app.js
+++ 02. another_app/components/app.js
@@ -3,23 +3,29 @@ class App extends React.Component {
         value: ""
     };
 
-    handleChangeText = (e) => {
+    handleChange = (e) => {
+        let result = ("Zawartość w evencie: " + e.target.value + "\n"
+            + "Zawartość value przed setState: " + this.state.value);
         this.setState({
             value: e.target.value
         });
+        result += ("\nZawartość value po setState: " + this.state.value);
+        console.log(result);
     };
 
-    handleResetText = () => {
+    handleClick = () => {
         this.setState({
             value: ""
         });
     };
 
     render() {
+        // render jest asynchroniczna, w chwili wywołania render zmienia sie stan
+        console.log("Zawartość value w trakcie metody render: " + this.state.value);
         return (
             <React.Fragment>
-                <input value={this.state.value} placeholder="wpisz tekst..." onChange={this.handleChangeText} type="text" />
-                <button onClick={this.handleResetText} >RESET</button>
+                <input value={this.state.value} placeholder="wpisz tekst..." onChange={this.handleChange} type="text" />
+                <button onClick={this.handleClick} >RESET</button>
                 <h1 className="title">{this.state.value.toUpperCase()}</h1>
             </React.Fragment>
         )
