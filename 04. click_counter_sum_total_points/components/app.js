93dfb44cab91fd4ee4927bb53efa9ff49d4d4af1
Krystian Petek <krystianpetek2@gmail.com>
2022-08-14 20:35:38 +0200

    app clickCounter and sumTotalPoints

---
+++ 04. click_counter_sum_total_points/components/app.js
@@ -0,0 +1,46 @@
+class Counter extends React.Component {
+    state = {
+        count: 0,
+        result: this.props.result
+    }
+
+    handleMathClick(type = "reset", number = 1) {
+        if (type === "substraction") {
+            this.setState((prevState) => ({
+                count: prevState.count + 1,
+                result: prevState.result - number
+            }))
+        }
+        else if (type === "addition") {
+            this.setState((prevState) => ({
+                count: prevState.count + 1,
+                result: prevState.result + number
+            }))
+        }
+        else {
+            this.setState((prevState) => ({
+                result: this.props.result,
+                count: prevState.count + 1
+            }));
+        }
+
+    }
+
+    render() {
+        return (
+            <>
+                <button onClick={this.handleMathClick.bind(this, "substraction", 10)}>-10</button>
+                <button onClick={this.handleMathClick.bind(this, "substraction", 1)}>-1</button>
+                <button onClick={() => this.handleMathClick()}>RESET</button>
+                <button onClick={() => this.handleMathClick("addition")}>+1</button>
+                <button onClick={this.handleMathClick.bind(this, "addition", 10)}>+10</button>
+                <h1>Liczba kliknięć: {this.state.count}</h1>
+                <h1>Wynik: {this.state.result}</h1>
+            </>
+        )
+    }
+
+}
+
+const root = document.getElementById('root');
+ReactDOM.render(<Counter result={10} />, root);
