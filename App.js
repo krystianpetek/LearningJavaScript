29cf811b71e1caf7c5e3ae6995be622bedf77c8e
Krystian Petek <krystianpetek2@gmail.com>
2022-08-14 13:43:32 +0200

    added css styles, show first JSX code in app by reactDom

--- App.js
+++ App.js
@@ -9,6 +9,32 @@ class App extends React.Component {
             </div>
         );
     }
-}
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const header = (<h1 className="title">Witaj na stronie!</h1>);
+
+const classBig = "big";
+
+const handleClick = () => alert("kliknął");
+
+const main = (
+    <div>
+        <h1 onClick={handleClick} className="red">Pierwszy artykuł</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, illo molestias? Fugit dolorum ab totam tenetur. Temporibus odit delectus velit aliquam eum officia, earum vel nihil esse? Accusantium, corrupti quibusdam.</p>
+    </div>
+);
+
+const text="Stopka...";
+const largeText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut error amet illum molestias, facere obcaecati commodi! Aliquam culpa sint quo voluptates numquam obcaecati ab laboriosam, debitis optio ratione eius dolorum.";
+const footer = (
+    <footer>
+        <p className={classBig}>{text}</p>
+        {largeText}
+    </footer>
+);
+
+const app = [header,main,footer];
+
+ReactDOM.render(app, document.getElementById("root"));
