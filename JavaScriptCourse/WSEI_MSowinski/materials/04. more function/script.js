a15135c4167d58cfc872257dd33a775abb3e8b02
Krystian Petek <krystianpetek2@gmail.com>
2022-10-02 21:46:34 +0200

    WSEI about hoisting, and over all about functions, variables scope global and local, call stack

---
+++ JavaScriptCourse/WSEI_MSowinski/materials/04. more function/script.js
@@ -0,0 +1,41 @@
+doTea(); // ok, hoisting
+
+function doTea() {
+    console.log('pour water into keetle');
+    console.log('pour the tea into a glass');
+    console.log('boil the water');
+    console.log('pour the tea');
+}
+
+
+function sayGlobalName() {
+    console.log(`global ${name}`);
+}
+function sayLocalName() {
+    var name = 'Janek';
+    console.log(`local ${name}`);
+}
+
+var name = 'Adam'; // zmiennna globalna name
+sayGlobalName();
+sayLocalName();
+console.log(name);
+
+// call stack
+function foo() {
+    let i = 0;
+    let j = bar(i);
+    return j;
+}
+function bar(i) {
+    return i + 5;
+}
+function basic() {
+    let j = foo();
+    return j;
+}
+basic();
+// JavaScript context => basic => foo => bar => bar => foo => basic => and finally JavaScript context
+
+tea(); // error, used before init
+const tea = doTea;
