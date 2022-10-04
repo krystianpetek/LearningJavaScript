ba14eebc042d6bf7ee588981dbe2cb03dc325180
Krystian Petek <krystianpetek2@gmail.com>
2022-10-04 21:39:16 +0200

    WSEI JS task 8 and 9 complete and all tasks completed

--- JavaScriptCourse/WSEI_MSowinski/materials/08. Zdarzenia w JS/materialy/js/zadanie08.js
+++ JavaScriptCourse/WSEI_MSowinski/materials/08. Zdarzenia w JS/materialy/js/zadanie08.js
@@ -1,3 +1,17 @@
-/**
- * Created by Jacek on 2016-01-12.
- */
+document.addEventListener('DOMContentLoaded', () => {
+
+    const resize = (x, y) => {
+        x.innerHTML = this.innerWidth + "px";
+        y.innerHTML = this.innerHeight + "px";
+    }
+
+    const windowWidth = document.querySelector('#windowWidth');
+    const windowHeight = document.querySelector('#windowHeight');
+    resize(windowWidth, windowHeight);
+
+    window.addEventListener('resize', function () {
+        resize(windowWidth, windowHeight);
+    })
+
+
+});
--- JavaScriptCourse/WSEI_MSowinski/materials/08. Zdarzenia w JS/materialy/js/zadanie09.js
+++ JavaScriptCourse/WSEI_MSowinski/materials/08. Zdarzenia w JS/materialy/js/zadanie09.js
@@ -1,28 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var a = document.getElementById("a");
-    var b = document.getElementById("b");
+document.addEventListener("DOMContentLoaded", function () {
+  var a = document.getElementById("a");
+  var b = document.getElementById("b");
 
-    // Tutaj this wskazuje na obiekt document (Ponieważ kontext wywołania funkcji
-    // addEventListener to właśnie document)
-    console.log("obiekt document: ", this);
+  // Tutaj this wskazuje na obiekt document (Ponieważ kontext wywołania funkcji
+  // addEventListener to właśnie document)
+  console.log("obiekt document: ", this);
 
-    a.addEventListener("click", function(event){
-      // Tutaj this wskazuje na element DOM o id a
-      console.log("a: ", this);
-    });
+  a.addEventListener("click", function (event) {
+    // Tutaj this wskazuje na element DOM o id a
+    console.log("a: ", this);
+  });
 
-    b.addEventListener("click", function(event){
-      // Tutaj this wskazuje na element DOM o id b
-      console.log("b: ", this);
+  b.addEventListener("click", function (event) {
+    // Tutaj this wskazuje na element DOM o id b
+    innerFuncOne(event.target);
+    console.log("b: ", this);
 
 
-      function innerFuncOne(){
-          // Tutaj this wskazuje na element Window, bo funkcja została
-          //wywołana bez żadnego kontekstu.
-          console.log("innerFuncOne: ", this);
+    function innerFuncOne(obj) {
+      // Tutaj this wskazuje na element Window, bo funkcja została
+      //wywołana bez żadnego kontekstu.
+      if (obj) {
+        console.log("innerFuncOne: ", obj)
       }
-      innerFuncOne();
 
-    });
+      console.log("innerFuncOne: ", this);
+    }
+
+  });
 
 })
