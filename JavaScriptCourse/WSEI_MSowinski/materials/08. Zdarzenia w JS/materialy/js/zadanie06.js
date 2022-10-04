30d828db7230ca349c79e8c845e999aa1ba7fb4c
Krystian Petek <krystianpetek2@gmail.com>
2022-10-04 21:15:20 +0200

    WSEI JS Task 6 and 7 completed

--- JavaScriptCourse/WSEI_MSowinski/materials/08. Zdarzenia w JS/materialy/js/zadanie06.js
+++ JavaScriptCourse/WSEI_MSowinski/materials/08. Zdarzenia w JS/materialy/js/zadanie06.js
@@ -1,3 +1,11 @@
-/**
- * Created by Jacek on 2016-01-12.
- */
+document.addEventListener('DOMContentLoaded', () => {
+
+    const container = document.querySelector('div#box');
+    container.addEventListener('mousemove', (event) => {
+        document.querySelector('#globalX').innerHTML = event.screenX;
+        document.querySelector('#globalY').innerHTML = event.screenY;
+        document.querySelector('#localX').innerHTML = event.clientX;
+        document.querySelector('#localY').innerHTML = event.clientY;
+    });
+
+});
