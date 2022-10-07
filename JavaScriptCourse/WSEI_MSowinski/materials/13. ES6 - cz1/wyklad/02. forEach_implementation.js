d19275bc0a77a5817f3cb34f21624cec38624128
Krystian Petek <krystianpetek2@gmail.com>
2022-10-07 22:21:07 +0200

    WSEI JS Chapter 13 - Lecturer, Array.forEach() implementation

---
+++ JavaScriptCourse/WSEI_MSowinski/materials/13. ES6 - cz1/wyklad/02. forEach_implementation.js
@@ -0,0 +1,23 @@
+// node 02. operationsOrArraysNext.js
+/* jshint -W104 */
+
+// Array.forEach()
+const arrayOfNumber = [1, 2, 3];
+function fn(element, index, array) {
+    console.log('[' + index + '] = ' + element);
+}
+arrayOfNumber.forEach(fn);
+
+// forEach() implementation
+Array.prototype.forEach2 = function (func) { // we extend object Array by one method using prototype
+    if (typeof func !== 'function') { // we check using 'typeof' if  that parameter is function
+        throw (error); // if parameter isn't function, throw Exception
+    }
+    let length = this.length; // we take the length
+
+    for (let i = 0; i < length; i++) { // iterate though array
+        func(this[i], i, this); // call function
+    }
+};
+
+arrayOfNumber.forEach2((e, i) => console.log(e + ' = array[' + i + ']'));
