7c85c0b73a00082db05a0c6490220e84a6efb438
Krystian Petek <krystianpetek2@gmail.com>
2022-10-09 22:08:21 +0200

    WSEI CHAPTER 14 - ES6 (4_Classes), Completed tasks from 1 to 5

--- JavaScriptCourse/WSEI_MSowinski/materials/14. ES6 - cz2/ES6 - 2/4_Classes/js/app.js
+++ JavaScriptCourse/WSEI_MSowinski/materials/14. ES6 - cz2/ES6 - 2/4_Classes/js/app.js
@@ -1,4 +1,5 @@
-{// Preparation
+{
+    console.log('preparation task')
     class Vehicle {
         constructor(name) {
             this.name = name;
@@ -38,4 +39,70 @@
     const boat = new Boat().boating();
     const car = new Car().driving();
     const plane = new Plane().flying();
+}
+
+
+{
+    console.log('\ntask 01 and task 04');
+    class Duck {
+        constructor() {
+            this.type = 'common duck';
+        }
+        quack() {
+            console.log('quack quack');
+        }
+        swim() {
+            console.log('i`m swimming...');
+        }
+        show() {
+            console.log('looks like ' + this.type);
+            this.fly();
+        }
+        fly() {
+            console.log('i`m flying...');
+        }
+    }
+
+    const duck = new Duck();
+    duck.quack();
+    duck.swim();
+    duck.show();
+
+    console.log('\ntask 02');
+    class WildDuck extends Duck {
+        constructor() {
+            super();
+            this.type = 'wild duck';
+        }
+    }
+    const wildDuck = new WildDuck();
+    wildDuck.quack();
+    wildDuck.swim();
+    wildDuck.show();
+
+    console.log('\ntask 03');
+    class CrossedDuck extends Duck {
+        constructor() {
+            super();
+            this.type = 'crossed duck';
+        }
+    }
+    const crossedDuck = new CrossedDuck();
+    crossedDuck.quack();
+    crossedDuck.swim();
+    crossedDuck.show();
+
+    console.log('\ntask 05');
+    class GumDuck extends Duck {
+        constructor() {
+            super();
+            this.type = 'gum duck';
+            this.fly = () => console.log('gum duck doesn`t know how to fly');
+        }
+    }
+    const gumDuck = new GumDuck();
+    gumDuck.quack();
+    gumDuck.swim();
+    gumDuck.show();
+
 }
