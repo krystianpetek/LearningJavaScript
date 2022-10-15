9ecd044aabca9ff3470a9b798e34a05c767feab8
Krystian Petek <krystianpetek2@gmail.com>
2022-10-15 21:25:19 +0200

    WSEI MSowinski FrontendFrameworks - Lab2 added react app with routers between 2 pages

---
+++ ReactCourse/WSEI MSowinski - FrontendFrameworks/Lab2/lab2/src/App.tsx
@@ -0,0 +1,22 @@
+import {FC} from "react";
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+
+import {Home} from "./components/routes/home/home";
+import {Posts} from "./components/routes/posts/posts";
+
+export const App:FC = () => {
+  return(
+    <BrowserRouter>
+      <img src={`${process.env.PUBLIC_URL}/img/logo.png`} alt="" />
+      <div>
+        <>{console.log("test")}</>
+        <Link to="/">Home</Link>
+        <Link to="posts">Posts</Link>
+      </div>
+      <Routes>
+        <Route path="posts" element={ <Posts/> }/>
+        <Route index element={ <Home/> } />
+      </Routes>
+    </BrowserRouter>
+  );
+}
