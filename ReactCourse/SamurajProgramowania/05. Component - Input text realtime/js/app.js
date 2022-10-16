47ed6373fb05d5a027d9747383ecbe0404496661
Krystian Petek <krystianpetek2@gmail.com>
2022-10-16 09:13:26 +0200

    ReactCourse (SamurajProgramowania) - 05. Component - Input text is displaying in realtime, setState text - before event, after event, before setState, after setState

--- ReactCourse/SamurajProgramowania/05. Component - Input text realtime/js/app.js
+++ ReactCourse/SamurajProgramowania/05. Component - Input text realtime/js/app.js
@@ -1,16 +1,16 @@
 class App extends React.Component {
     state = {
         value: ""
-    };
+    }
 
     handleChange = (e) => {
-        let result = ("Zawartość w evencie: " + e.target.value + "\n"
-            + "Zawartość value przed setState: " + this.state.value);
+        let result = ("Zawartość w evencie: " + e.target.value + "\n");
+        result += ("Zawartość value przed setState: " + this.state.value + "\n");
+        result += ("Zawartość value po setState: " + this.state.value);
+        console.log(result);
         this.setState({
             value: e.target.value
         });
-        result += ("\nZawartość value po setState: " + this.state.value);
-        console.log(result);
     };
 
     handleClick = () => {
@@ -24,13 +24,13 @@ class App extends React.Component {
         console.log("Zawartość value w trakcie metody render: " + this.state.value);
         return (
             <React.Fragment>
-                <input value={this.state.value} placeholder="wpisz tekst..." onChange={this.handleChange} type="text" />
+                <input type="text" value={this.state.value} placeholder="wpisz tekst..." onChange={this.handleChange} />
                 <button onClick={this.handleClick} >RESET</button>
                 <h1 className="title">{this.state.value.toUpperCase()}</h1>
             </React.Fragment>
         )
-    };
-};
+    }
+}
 
 const root = document.getElementById('root');
 ReactDOM.render(<App />, root);
