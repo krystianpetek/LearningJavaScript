37ded259ab1dd8149476812be038a001bb61108b
Krystian Petek <krystianpetek2@gmail.com>
2022-10-16 09:55:06 +0200

    ReactCourse (SamurajProgramowania) - 06. Component - Button show/hide using class state instead class or something other

--- ReactCourse/SamurajProgramowania/06. Component - Button show hide/js/app.js
+++ ReactCourse/SamurajProgramowania/06. Component - Button show hide/js/app.js
@@ -5,17 +5,18 @@ class Message extends React.Component {
             messageIsActive: false
         };
         this.handleMessageButton = this.handleMessageButton.bind(this);
-    };
+    }
 
     handleMessageButton = () => {
         this.setState((prevState) => ({
             messageIsActive: !prevState.messageIsActive
         }));
-        // debugger
+        debugger
     };
 
     render() {
         const text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi, perferendis. Perspiciatis repellendus ipsam repellat ratione ab nobis earum, quisquam, recusandae accusantium numquam optio nostrum reiciendis doloribus soluta, itaque animi possimus! ";
+
         console.log(this.state.messageIsActive);
         return (
             <React.Fragment>
