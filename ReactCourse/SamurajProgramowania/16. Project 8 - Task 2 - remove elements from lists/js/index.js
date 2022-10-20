b6850d9fcaaad2e3f03f376fa7c236368da5a44b
Krystian Petek <krystianpetek2@gmail.com>
2022-10-20 07:06:16 +0200

    ReactCourse (SamurajProgramowania) - 16. Project 8 - Task 2 - remove elements from lists

---
+++ ReactCourse/SamurajProgramowania/16. Project 8 - Task 2 - remove elements from lists/js/index.js
@@ -0,0 +1,73 @@
+class List extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            listOfPeople: [
+                {
+                    id: 1,
+                    name: "Patrycja P."
+                }, {
+                    id: 2,
+                    name: "Krystian P."
+                }, {
+                    id: 3,
+                    name: "Józef P."
+                }, {
+                    id: 4,
+                    name: "Teresa P."
+                }, {
+                    id: 5,
+                    name: "Wojciech W."
+                }
+            ]
+        }
+    }
+
+    handleDeletePerson = (props) => {
+        const people = this.state.listOfPeople.filter(people => people.id != props);
+        this.setState({
+            listOfPeople: people
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Person
+                    people={this.state.listOfPeople}
+                    delete={this.handleDeletePerson} />
+            </div>
+        )
+    }
+}
+
+class Person extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const people = this.props.people.map(person =>
+            <li
+                key={person.id}>{person.name}
+                <input
+                    type="button"
+                    value="Delete"
+                    onClick={this.props.delete.bind(this, person.id)} />
+            </li>
+        );
+        return (
+            <ul>
+                {people}
+            </ul>
+        )
+    }
+}
+
+const rootDOM = document.querySelector("#root");
+const rootReact = ReactDOM.createRoot(rootDOM);
+rootReact.render(
+    <React.StrictMode>
+        <List></List>
+    </React.StrictMode>
+)
