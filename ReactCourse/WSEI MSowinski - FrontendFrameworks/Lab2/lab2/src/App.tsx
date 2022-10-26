53a488693dcffcf756f25f0fe3908f54b672b4f9
Krystian Petek <krystianpetek2@gmail.com>
2022-10-26 18:12:42 +0200

    Added changes in projects from previous FrontendFrameworks labs after practice in react

--- ReactCourse/WSEI MSowinski - FrontendFrameworks/Lab2/lab2/src/App.tsx
+++ ReactCourse/WSEI MSowinski - FrontendFrameworks/Lab2/lab2/src/App.tsx
@@ -1,21 +1,20 @@
-import {FC} from "react";
-import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import { FC } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import {Home} from "./components/routes/home/home";
-import {Posts} from "./components/routes/posts/posts";
+import { Home } from "./components/routes/home/home";
+import { Posts } from "./components/routes/posts/posts";
 
-export const App:FC = () => {
-  return(
+export const App: FC = () => {
+  return (
     <BrowserRouter>
-      <img src={`${process.env.PUBLIC_URL}/img/logo.png`} alt="" />
       <div>
         <>{console.log("test")}</>
         <Link to="/">Home</Link>
         <Link to="posts">Posts</Link>
       </div>
       <Routes>
-        <Route path="posts" element={ <Posts/> }/>
-        <Route index element={ <Home/> } />
+        <Route path="posts" element={<Posts />} />
+        <Route index element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
