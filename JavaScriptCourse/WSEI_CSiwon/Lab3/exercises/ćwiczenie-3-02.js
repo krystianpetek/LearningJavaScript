160e01f7e4ddcc418672dcd2ef5afba05973d64c
Krystian Petek <krystianpetek2@gmail.com>
2022-11-12 06:55:40 +0100

    WSEI CSiwon JS - Lab3 tasks completed, but cw4 to correct

---
+++ JavaScriptCourse/WSEI_CSiwon/Lab3/exercises/ćwiczenie-3-02.js
@@ -0,0 +1,31 @@
+/**
+ * Zdefiniuj funkcję, która generuje kolejne liczby parzyste większę lub równe `n` i mniejsze lub równe `m`.
+ */
+
+function* evenNumbers(n, m) {
+    while (n < m) {
+        if (n % 2 === 0) {
+            yield n;
+            n += 2;
+        }
+        else {
+            n += 1;
+        }
+    }
+}
+
+let sum = 0;
+for (let item of evenNumbers(1, 9)) {
+    sum += item;
+}
+if (sum === 2 + 4 + 6 + 8) {
+    console.log("Test 21 passed.");
+} else {
+    console.log("Test 21 failed.");
+}
+let gen = evenNumbers(1, 5)
+if (gen.next().value == 2 && gen.next().value == 4 && gen.next().done) {
+    console.log("Test 22 passed.");
+} else {
+    console.log("Test 22 failed.");
+}
