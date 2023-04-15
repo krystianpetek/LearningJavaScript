3f66af47d4db093f50baf5c207232d7c4cbb196f
Krystian Petek <krystianpetek2@gmail.com>
2023-04-15 09:54:01 +0200

    AngularCourse - created dynamically  component using directive and ng-template

--- AngularCourse/Packt_LearningAngular/chapter3_4-modules_components/src/app/products/products.module.ts
+++ AngularCourse/Packt_LearningAngular/chapter3_4-modules_components/src/app/products/products.module.ts
@@ -4,9 +4,10 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductComponent } from './product/product.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { SortPipe } from './sort.pipe';
+import { ProductHostDirective } from './product-host.directive';
 
 @NgModule({
-  declarations: [ProductListComponent, ProductDetailComponent, SortPipe],
+  declarations: [ProductListComponent, ProductDetailComponent, SortPipe, ProductHostDirective],
   imports: [CommonModule, ProductComponent],
   exports: [ProductListComponent],
 })
