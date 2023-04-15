4edf655de0cf9d5f712a87f64fe0fdc37f57c25b
Krystian Petek <krystianpetek2@gmail.com>
2023-04-15 20:10:12 +0200

    AngularCourse - Added AppConfig and injected by InjectionToken to DI container to global AppComponent, completed chapter 6

--- AngularCourse/Packt_LearningAngular/chapter3_4-modules_components/src/app/app.component.ts
+++ AngularCourse/Packt_LearningAngular/chapter3_4-modules_components/src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
+import { APP_CONFIG, AppConfig, appSettings } from './app.config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [
+    {
+      provide: APP_CONFIG,
+      useValue: appSettings,
+    },
+  ],
 })
 export class AppComponent implements OnDestroy {
+  constructor(@Inject(APP_CONFIG) config: AppConfig) {}
+
   ngOnDestroy(): void {}
   title = 'chapter3-modules';
   today = new Date();
