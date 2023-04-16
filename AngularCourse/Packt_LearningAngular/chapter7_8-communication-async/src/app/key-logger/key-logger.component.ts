449745beb312a03ad5d338cc14b161c376fb4ba8
Krystian Petek <krystianpetek2@gmail.com>
2023-04-16 10:16:17 +0200

    AngularCourse - added observable using 'of()' statement to convert args to observable sequence and 'from([])' to convert anything to observable

--- AngularCourse/Packt_LearningAngular/chapter7_8-communication-async/src/app/key-logger/key-logger.component.ts
+++ AngularCourse/Packt_LearningAngular/chapter7_8-communication-async/src/app/key-logger/key-logger.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { fromEvent } from 'rxjs';
+import { Observable, Subscription, from, fromEvent, of } from 'rxjs';
 
 @Component({
   selector: 'app-key-logger',
@@ -9,6 +9,9 @@ import { fromEvent } from 'rxjs';
 export class KeyLoggerComponent implements OnInit {
   public keys = '';
   @ViewChild('keyContainer', { static: true }) input: ElementRef | undefined;
+  public values = new Observable<number>();
+  private ofSubscribe?: Subscription;
+  private fromSubscribe?: Subscription;
 
   ngOnInit(): void {
     const logger$ = fromEvent<KeyboardEvent>(
@@ -16,5 +19,20 @@ export class KeyLoggerComponent implements OnInit {
       'keyup'
     );
     logger$.subscribe((event) => (this.keys += event.key));
+
+    this.values = of(1, 2, 3);
+    this.ofSubscribe = this.values.subscribe((value) =>
+      console.log(`of(${value}), ${value}`)
+    );
+
+    this.values = from([1, 2, 3]);
+    this.fromSubscribe = this.values.subscribe((value) => {
+      console.log(`from([${value}]), ${value}`);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.fromSubscribe) this.fromSubscribe.unsubscribe();
+    if (this.ofSubscribe) this.ofSubscribe.unsubscribe();
   }
 }
