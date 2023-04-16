5084fdcede25e1a2041da2c4bee4d18c415e5378
Krystian Petek <krystianpetek2@gmail.com>
2023-04-16 13:23:05 +0200

    AngularCourse - unsubscribe observer automatically using async pipe in ProductListComponent

--- AngularCourse/Packt_LearningAngular/chapter7_8-communication-async/src/app/products/product-list/product-list.component.ts
+++ AngularCourse/Packt_LearningAngular/chapter7_8-communication-async/src/app/products/product-list/product-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { ProductsService } from '../products.service';
 import { ProductViewService } from '../product-view/product-view.service';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -16,19 +16,15 @@ import { Subscription } from 'rxjs';
 })
 export class ProductListComponent implements OnInit, OnDestroy {
   private readonly _productService: ProductsService;
-  private _productSubscription?: Subscription | undefined;
-  public products: Product[] = [];
+  private _productSubscription?: Subscription;
+  public products$?: Observable<Product[]>;
 
   public constructor(productService: ProductsService) {
     this._productService = productService;
   }
 
   private getProducts(): void {
-    this._productSubscription = this._productService
-      .getProducts()
-      .subscribe((products) => {
-        this.products = products;
-      });
+    this.products$ = this._productService.getProducts();
   }
 
   ngOnInit(): void {
