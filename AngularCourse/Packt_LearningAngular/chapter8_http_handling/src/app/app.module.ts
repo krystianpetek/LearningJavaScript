8be752962300d1855df2f6a375eb2b845635fbe8
Krystian Petek <krystianpetek2@gmail.com>
2023-04-16 17:57:16 +0200

    AngularCourse - added HttpClientModule to project, changed name for first application from chapter 1 and 2

--- AngularCourse/Packt_LearningAngular/chapter8_http_handling/src/app/app.module.ts
+++ AngularCourse/Packt_LearningAngular/chapter8_http_handling/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductsModule } from './products/products.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ProductsModule],
+  imports: [BrowserModule, ProductsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
