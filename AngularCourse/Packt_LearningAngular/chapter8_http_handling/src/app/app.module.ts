a0882ecd7acdfa0ac7e48afd772db873d6b37644
Krystian Petek <krystianpetek2@gmail.com>
2023-04-19 14:10:06 +0200

    AngularCourse - added authorizing for http request using auth interceptor

--- AngularCourse/Packt_LearningAngular/chapter8_http_handling/src/app/app.module.ts
+++ AngularCourse/Packt_LearningAngular/chapter8_http_handling/src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
+import { AuthInterceptor } from './auth.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, ProductsModule, HttpClientModule, AuthModule],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ AngularCourse/Packt_LearningAngular/chapter8_http_handling/src/app/auth.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth/auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const authRequest = request.clone({
+      setHeaders: {
+        Authorization: 'myAuthToken',
+      },
+    });
+    return next.handle(authRequest);
+  }
+}
