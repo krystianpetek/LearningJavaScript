7162917dabdfd805ff62f451fdbacbe11dc4982f
Krystian Petek <krystianpetek2@gmail.com>
2023-04-20 21:21:56 +0200

    AngularCourse - added navigating to a route on button click imperatively and changed routerLink to array syntax in page not found component

--- AngularCourse/Packt_LearningAngular/chapter9_routing/src/app/page-not-found/page-not-found.component.ts
+++ AngularCourse/Packt_LearningAngular/chapter9_routing/src/app/page-not-found/page-not-found.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.css']
+  styleUrls: ['./page-not-found.component.css'],
 })
 export class PageNotFoundComponent {
+  private _router: Router;
 
+  public constructor(router: Router) {
+    this._router = router;
+  }
+  public goHome(): void {
+    this._router.navigate(['/']);
+  }
 }
