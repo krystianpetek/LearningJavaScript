0ca40f53e28069a9524b260de801493c5eae4c2d
Krystian Petek <krystianpetek2@gmail.com>
2023-04-23 09:21:43 +0200

    AngularCourse - added types for productForm in create product component, Created form by formBuilderr

--- AngularCourse/Packt_LearningAngular/chapter10_forms/src/app/products/product-create/product-create.component.ts
+++ AngularCourse/Packt_LearningAngular/chapter10_forms/src/app/products/product-create/product-create.component.ts
@@ -1,8 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { ProductsService } from '../products.service';
-import { Observable } from 'rxjs';
 import { Product } from '../models/product';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-product-create',
@@ -11,36 +10,75 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ProductCreateComponent {
   private _productsService: ProductsService;
-  @Output() added = new EventEmitter<Product>();
-  public productForm = new FormGroup({
-    name: new FormControl('', { nonNullable: true }),
+  private _formBuilder: FormBuilder;
+
+  @Output() public added = new EventEmitter<Product>();
+
+  public productForm = new FormGroup<{
+    name: FormControl<string>;
+    price: FormControl<number | undefined>;
+    info: FormGroup<{
+      category: FormControl<string>;
+      description: FormControl<string>;
+      image: FormControl<string>;
+    }>;
+  }>({
+    name: new FormControl<string>('', { nonNullable: true }),
     price: new FormControl<number | undefined>(undefined, {
       nonNullable: true,
     }),
-    info: new FormGroup({
-      category: new FormControl(''),
-      description: new FormControl(''),
-      image: new FormControl(''),
+    info: new FormGroup<{
+      category: FormControl<string>;
+      description: FormControl<string>;
+      image: FormControl<string>;
+    }>({
+      category: new FormControl<string>('', { nonNullable: true }),
+      description: new FormControl<string>('', { nonNullable: true }),
+      image: new FormControl<string>('', { nonNullable: true }),
     }),
   });
 
-  public constructor(productsService: ProductsService) {
-    this._productsService = productsService;
+  private buildForm() {
+    this.productForm = this._formBuilder.nonNullable.group({
+      name: this._formBuilder.nonNullable.control<string>(''),
+      price: this._formBuilder.nonNullable.control<number | undefined>(
+        undefined,
+        {}
+      ),
+      info: this._formBuilder.nonNullable.group<{
+        category: FormControl<string>;
+        description: FormControl<string>;
+        image: FormControl<string>;
+      }>({
+        category: this._formBuilder.nonNullable.control<string>(''),
+        description: this._formBuilder.nonNullable.control<string>(''),
+        image: this._formBuilder.nonNullable.control<string>(''),
+      }),
+    });
   }
 
-  createProduct(name: string, price: number): void {
+  public constructor(
+    productsService: ProductsService,
+    formBuilder: FormBuilder
+  ) {
+    this._productsService = productsService;
+    this._formBuilder = formBuilder;
+  }
+
+  public createProduct(name: string, price: number): void {
     this._productsService.addProduct(name, price).subscribe((product) => {
       this.added.emit(product);
     });
   }
 
-  get name(): FormControl<string> {
+  private get name(): FormControl<string> {
     return this.productForm.controls.name;
   }
-  get price(): FormControl<number | undefined> {
+  private get price(): FormControl<number | undefined> {
     return this.productForm.controls.price;
   }
-  createProductReactive(): void {
+
+  public createProductReactive(): void {
     this._productsService
       .addProduct(this.name.value, Number(this.price.value))
       .subscribe((product) => {
