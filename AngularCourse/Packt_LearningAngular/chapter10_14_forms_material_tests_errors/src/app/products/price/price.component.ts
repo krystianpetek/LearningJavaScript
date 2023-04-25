dc86e67ae8a2312fcb1aa2294dec501c3f42e2df
Krystian Petek <krystianpetek2@gmail.com>
2023-04-25 17:15:42 +0200

    AngularCourse - closed change price dialog by reference to this

--- AngularCourse/Packt_LearningAngular/chapter10_14_forms_material_tests_errors/src/app/products/price/price.component.ts
+++ AngularCourse/Packt_LearningAngular/chapter10_14_forms_material_tests_errors/src/app/products/price/price.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-price',
@@ -7,10 +7,19 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./price.component.css'],
 })
 export class PriceComponent {
+  private _dialogRef: MatDialogRef<PriceComponent>;
   public data: number;
   public price: number | undefined;
 
-  public constructor(@Inject(MAT_DIALOG_DATA) data: number) {
+  public constructor(
+    @Inject(MAT_DIALOG_DATA) data: number,
+    dialogRef: MatDialogRef<PriceComponent>
+  ) {
     this.data = data;
+    this._dialogRef = dialogRef;
+  }
+
+  public save(): void {
+    this._dialogRef.close(this.price);
   }
 }
