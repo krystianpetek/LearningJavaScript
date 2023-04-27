6e9fb243e465f3b9df90f7417d37a55c550410d0
Krystian Petek <krystianpetek2@gmail.com>
2023-04-27 19:36:07 +0200

    AngularCourse - created first unit tests not associated with our project

--- AngularCourse/Packt_LearningAngular/chapter10_14_forms_material_tests_errors/src/app/app.component.spec.ts
+++ AngularCourse/Packt_LearningAngular/chapter10_14_forms_material_tests_errors/src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -30,6 +26,29 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('chapter9_routing app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'chapter9_routing app is running!'
+    );
+  });
+});
+
+describe('Calculator', () => {
+  let total: number;
+  beforeEach(() => {
+    total = 1;
+  });
+
+  it('should add two numbers', () => {
+    total = total + 1;
+    expect(total).toBe(2);
+  });
+
+  it('should subtract two numbers', () => {
+    total = total - 1;
+    expect(total).toBe(0);
+  });
+
+  afterEach(() => {
+    total = 0;
   });
 });
