3b2e50cb7db6406c085c9eed8193cee1a3a3e16a
Krystian Petek <krystianpetek2@gmail.com>
2023-04-28 16:14:43 +0200

    AngularCourse  - created test service with dependencies on products service

--- AngularCourse/Packt_LearningAngular/chapter10_14_forms_material_tests_errors/src/app/products/products.service.spec.ts
+++ AngularCourse/Packt_LearningAngular/chapter10_14_forms_material_tests_errors/src/app/products/products.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { ProductsService } from './products.service';
+import { ProductDto } from './models/product-dto';
 
 describe('ProductsService', () => {
   let service: ProductsService;
-
+  let httpTestingController: HttpTestingController;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(ProductsService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should get products', () => {
+    service.getProducts().subscribe();
+    const request = httpTestingController.expectOne(
+      'https://fakestoreapi.com/products'
+    );
+    expect(request.request.method).toBe('GET');
+  });
+
+  it('should add a product', () => {
+    service.addProduct('Fake product', 100).subscribe();
+    const request = httpTestingController.expectOne(
+      'https://fakestoreapi.com/products'
+    );
+    expect(request.request.method).toBe('POST');
+    expect(request.request.body).toEqual({
+      title: 'Fake product',
+      price: 100,
+    });
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 });
